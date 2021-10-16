e04787ac5face140067f55a203d083ae0dee4e4d
Danny Limoges <drlimoges@gmail.com>
2021-10-15 20:09:13 -0500

    WORKS

--- api/openfda.py
+++ api/openfda.py
@@ -44,37 +44,16 @@ def test():
         ,'limit': '5'
     }
 
-    approach = 4
 
-    if approach == 1:
-        data2 = requests.get(URL2).json()
-        print(data2)
-        for result in data2.get('results', []):
-            print(result)
-    elif approach == 2:
-        data2 = requests.get(URL2a, params=params2b).json()
-        print(data2)
-        for result in data2.get('results', []):
-            print(result)
-    elif approach == 3:
-        prepped = requests.Request("GET", url=URL2).prepare()
-        s = requests.Session()
-        print (f'Prepped URL: {prepped.url}\nPrepped body: {prepped.body}')
+    payload_str = urllib.parse.urlencode(params2b, safe=':+')
+    prepped = requests.Request("GET", url=URL2a, params=payload_str).prepare()
+    s = requests.Session()
+    print (f'Prepped URL: {prepped.url}\nPrepped body: {prepped.body}')
 
-        data2 =  s.send(prepped).json()
-        #print(data2.content)
-        for result in data2.get('results', []):
-            print(result)
-    elif approach == 4:
-        payload_str = urllib.parse.urlencode(params2b, safe=':+')
-        prepped = requests.Request("GET", url=URL2a, params=payload_str).prepare()
-        s = requests.Session()
-        print (f'Prepped URL: {prepped.url}\nPrepped body: {prepped.body}')
-
-        data2 =  s.send(prepped).json()
-        #print(data2.content)
-        for result in data2.get('results', []):
-            print(result)
+    data2 =  s.send(prepped).json()
+    #print(data2.content)
+    for result in data2.get('results', []):
+        print(result)
 
 def _main(config_file = None):
     test()
