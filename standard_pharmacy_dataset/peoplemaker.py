aba288bc081483ab0d10bc244809c49e42afef86
Danny Limoges <drlimoges@gmail.com>
2022-04-13 20:02:38 -0500

    Researching

---
+++ standard_pharmacy_dataset/peoplemaker.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import random
+
+"""
+
+impact of social determinants of health on medication adherence:
+https://pubmed.ncbi.nlm.nih.gov/33515188/
+    food insecurity  (aOR = 0.56; 95% CI 0.42-0.7)
+    housing instability (aOR = 0.64; 95% CI 0.44-0.93)
+    social determinants overall (aOR = 0.75; 95% CI 0.65-0.88) 
+    
+Social Risk Factors for Medication Nonadherence: Findings from the CARDIA Study
+https://pubmed.ncbi.nlm.nih.gov/32019655/
+    income <$25,000 (OR = 2.37 [95% CI 1.12-4.98], p < .05)
+    high chronic stress (OR = 2.07 [95% CI 1.09-3.94], p < .05) 
+    Individuals with ≥3 social risk factors had >3 times higher odds of nonadherence than 
+        counterparts with no social risk factors (OR = 3.26 [95% CI 1.72-6.19], p < .001)
+
+"""
