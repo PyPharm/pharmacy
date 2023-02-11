a49210dcd5ff9754d77c944322284f51a3aec709
Danny Limoges <drlimoges@gmail.com>
2023-02-11 08:57:56 -0600

    Beginning "examples" folder

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
 #pypharm by Danny Limoges, PharmD
 # A construct for managing data for: fill claims, fills, prescriptions, drug data, insurance data and more.
 # The goal is to develop easy-to-use interfaces for pharmacy-related APIs
+
+
+
+
